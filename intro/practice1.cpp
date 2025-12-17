f0ee4dd185fa7410a0221f481fbc17be06cb47a2
VASU CHAND <vasuchand9@gmail.com>
2025-12-17 20:46:35 +0530

    add the practice of sum file

---
+++ intro/practice1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cout << "enter the value of a "<< endl;
+cin >> a;
+cout << "enter the value of b" << endl;
+cin >> b;
+int sum = a +b ;
+cout  << "sum is :" << sum << endl ;
+cout << "SUM IS :"<< a+b << endl;
+return 0;
+
+
+
+
+
+
+}
