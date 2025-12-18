014912486790d7cb8d8c9899c7013e669816a1ab
VASU CHAND <vasuchand9@gmail.com>
2025-12-18 10:48:25 +0530

    add whileloop practice

---
+++ loops/whileloop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+int count =1;
+cout << "enter the value of n" << endl;
+cin >>n;
+
+
+while(count<=n){
+    cout << count << endl;
+    count++;
+
+}
+ 
+cout << endl;
+
+
+}
