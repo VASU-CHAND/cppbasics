dc8996343e1910234db19954e1a318fef369330b
VASU CHAND <vasuchand9@gmail.com>
2025-12-18 07:58:47 +0530

    add the calcutor program

---
+++ intro/calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a , b;
+cout << "enter the value of a" << endl;
+cin >> a;
+cout << "enter the value of b" << endl;
+cin >> b;
+cout << "sum  :" << a+b << endl;
+cout << "diff :" << a-b << endl;
+cout << "mul  :" << a*b << endl;
+cout << "div  :" << a/b << endl;
+cout << "mod  :" << a%b << endl;
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+}
