bab275c20fb126ed2d86f2cda081a9c9fefa6179
VASU CHAND <vasuchand9@gmail.com>
2025-12-18 10:57:25 +0530

    add the for practice

---
+++ loops/for.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout << "enter the value of n" << endl;
+cin >> n;
+int i;
+for ( i=0 ; i<=n ; i++ ){
+
+cout << i << endl;
+
+}
+
+return 0;
+
+
+}
