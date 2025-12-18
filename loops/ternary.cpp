dee586407a592c2cea17cc7e4a19d8e46835f9a2
VASU CHAND <vasuchand9@gmail.com>
2025-12-18 10:29:40 +0530

    add the file

---
+++ loops/ternary.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+// condition ? stt1 : stt2 ;  if true then stement 1
+int n = 10;
+cout << ( n>=0 ? "positive" : "negative") << endl;
+return 0;
+
+
+
+}
