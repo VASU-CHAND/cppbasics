c300cc7dfbb71f6a847c3d85333de1bf8e2dd717
VASU CHAND <vasuchand9@gmail.com>
2025-12-20 11:02:59 +0530

    add correct pattern code

---
+++ patterns/correctcode1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i;
+int n;// n for row
+int m;// m for coloumn
+cout << "enter the value of n" << endl;
+cin >> n;
+cout << "enter the value of m" << endl;
+cin >> m;
+int num =0;
+for(i =0; i<m; i++){
+    int j=0; // ander taaki j wapis zero hojai
+    for(j; j<n; j++){
+    cout <<num ;
+    num++;
+    }
+  cout << endl;
+}
+return 0;
+
+
+}
