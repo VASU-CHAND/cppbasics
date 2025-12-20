0942bb2703d699eefcc74e708e325bb4bfef08e5
VASU CHAND <vasuchand9@gmail.com>
2025-12-20 12:44:46 +0530

    add the triangle pattern file

---
+++ patterns/trianglepat.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main(){
+
+int n;
+int i;
+int m;
+n=1;
+i=0;
+cout << "enter the value of m" <<endl;
+cin >> m;
+for(i; i<m; i++){
+int j =0;
+for(j; j<n; j++){
+
+cout << "*" ;
+  
+}
+ n++;
+cout << endl;
+}
+return 0;
+}
