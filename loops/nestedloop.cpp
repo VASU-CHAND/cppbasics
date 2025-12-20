7816991e0c4675c77556d9dd70b9e7eecfa178fb
VASU CHAND <vasuchand9@gmail.com>
2025-12-20 09:05:29 +0530

    add nested loop practice

---
+++ loops/nestedloop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i;
+int n=5;
+
+int j;
+for(i =0; i<n ; i++){
+    for(j=0; j<=n; j++){
+    cout <<"*";
+    }
+  cout << endl;
+}
+return 0;
+
+
+}
