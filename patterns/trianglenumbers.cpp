86d21606ae3c2ecd5bfa7312b0bac89fe1c31c4d
VASU CHAND <vasuchand9@gmail.com>
2025-12-20 15:48:13 +0530

    add triangle numbers file

---
+++ patterns/trianglenumbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int i;
+int j;
+int m;
+int n;
+cout << "enter the value of m" << endl;
+cin >> m;
+n=1;
+    i=0;
+     int l=1;
+for(i; i<m; i++){
+    
+     int j=0;
+    for (j; j<n; j++){
+    cout << l;
+
+    }
+    n++;
+   cout << endl;
+   l++;
+
+}
+}
