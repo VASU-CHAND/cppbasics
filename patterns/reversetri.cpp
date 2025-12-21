7e0a7102b95cf0169b6b459a64dcf8b59c29607a
VASU CHAND <vasuchand9@gmail.com>
2025-12-21 08:55:00 +0530

    add the reverse triangle patterm

---
+++ patterns/reversetri.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout << "enter the value of n" << endl;
+cin >> n;
+int i;
+
+for(i=0; i<n; i++){
+
+for(int j=i+1; j>0; j--){
+cout << j;
+}
+
+cout<< endl;
+}
+
+}
+
+
+
+
+
