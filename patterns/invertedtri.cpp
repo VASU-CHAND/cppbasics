f61b5cde20a62db1488f1fa805bfbcdfc18d1adf
VASU CHAND <vasuchand9@gmail.com>
2025-12-21 18:58:31 +0530

    add the new corrected code of inverted tri

--- patterns/invertedtri.cpp
+++ patterns/invertedtri.cpp
@@ -7,19 +7,16 @@ cin >> n;
 
 int num =1;
 for(int i=0; i<n; i++){
-
-    for(   int j=n;  j>i; j--){
-    for(   int k=0;  k<i; k++){
-    cout << " "; 
-     num++;
-    }
-    }
-    cout << num; 
- 
-    
+for(int j=0; j<i; j++){
+cout << " ";
+}
+for(int j=0; j<n-i; j++){
+    cout <<num ;
+   
+}
+ num++;
 cout << endl;
 }
-
 return 0;
 
 }
